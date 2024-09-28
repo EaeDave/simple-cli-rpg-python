230c3918b049938f366d71e5e2775c470ecdff55
EaeDave <EaeDav3@gmail.com>
2024-09-28 17:53:30 -0300

    Função e Loop - GERAR NPCs

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 from random import randint
 
-lista_npcs = []
+lista_npcs = []  # Lista inicialmente vazia
 
-def criar_monstro():
+# Função responsável por criar o NPC
+def criar_npc():
     level = randint(0, 50)
 
-    novo_npc = {
+    npc = {
         'nome': f'Monstro #{level}',
         'level': level,
         'dano': 5 * level,
         'hp': 100 * level,
     }
 
-    lista_npcs.append(novo_npc)
+    return npc
+
+
+# Função responsável por gerar os NPCs
+def gerar_npcs(numero_npcs):
+    for x in range(numero_npcs):
+        npc = criar_npc()
+        lista_npcs.append(npc)
+
+
+# Função responsável por exibir os NPCs
+def exibir_npcs():
+    for npc in lista_npcs:
+        print(f'Nome: {npc['nome']} | Level: {npc['level']} | Dano: {npc['dano']} | HP: {npc['hp']}')
+
