517025d6bc3835b620a83d55d9a001c4249ef68e
EaeDave <EaeDav3@gmail.com>
2024-09-28 18:09:34 -0300

    Função - ATACAR NPC

--- main.py
+++ main.py
@@ -2,15 +2,27 @@ from random import randint
 
 lista_npcs = []  # Lista inicialmente vazia
 
+player = {
+    'nome': 'Laerze',
+    'level': 1,
+    'exp': 0,
+    'exp_max': 50,
+    'hp': 100,
+    'hp_max': 100,
+    'dano': 25,
+
+}
+
 # Função responsável por criar o NPC
 def criar_npc():
-    level = randint(0, 50)
+    level = randint(0, 20)
 
     npc = {
         'nome': f'Monstro #{level}',
         'level': level,
         'dano': 5 * level,
         'hp': 100 * level,
+        'exp': 7 * level
     }
 
     return npc
@@ -27,4 +39,8 @@ def gerar_npcs(numero_npcs):
 def exibir_npcs():
     for npc in lista_npcs:
         print(f'Nome: {npc['nome']} | Level: {npc['level']} | Dano: {npc['dano']} | HP: {npc['hp']}')
+
+
+def atacar_npc(npc):
+    npc['hp'] -= player['dano']
 
